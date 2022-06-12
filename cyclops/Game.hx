05f1265b01ded2424af7e49b8c0ab15c70fd6b1e
inc0der <inc0der.dev@gmail.com>
2022-06-12 18:00:46 -0300

    add instance to Game class

--- cyclops/Game.hx
+++ cyclops/Game.hx
@@ -1,14 +1,33 @@
 package cyclops;
 
 class Game extends hxd.App {
-  public var scene: Scene;
+  public var scene(default, null): Scene;
+
+  @:isVar
+  public static var instance(get, null): Game;
+
+  public var onReady(default, set): Game->Void;
 
   public function new() {
     super();
+    instance = this;
+  }
+
+  public static function get_instance(): Game {
+    if (instance == null) {
+      instance = new Game();
+    }
+    return instance;
+  }
+
+  public function set_onReady(value) {
+    return onReady = value;
   }
 
   public override function init() {
-    trace("Hello World");
+    if (onReady != null) {
+      onReady(this);
+    }
     #if (hl && !debug)
     Res.initLocal();
     #else
